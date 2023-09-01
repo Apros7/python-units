a258ecbc9ead08ff7b75686881f67a99172a4087
apros7 <lucas.vilsen@gmail.com>
2023-09-01 13:14:43 +0200

    div and mul working

--- python_units/__init__.py
+++ python_units/__init__.py
@@ -5,8 +5,9 @@ from python_units.constants import UNITS, SPECIAL_UNITS, PREFIXES
 pi = math.pi
 
 ## Relevant functions ##
-def sqrt(value): pass
-def nsqrt(value): pass
+def sqrt(value, n=1): pass
+def nsqrt(value, n): pass
+def round(value): pass
 
 ## Value Class ##
 class v():
@@ -14,29 +15,67 @@ class v():
         self._get_value(value)
         self.value *= self.unit.get_prefix()
 
-
     def __str__(self): return str(self.value) + " " + self.unit.get()
     def __eq__(self, other): return str(self) == other
     def __add__(self, other): return v(str(self.value + other.value) + " " + self.unit.get_add(other))
     def __sub__(self, other): return v(str(self.value - other.value) + " " + self.unit.get_sub(other))
-    # def __mul__(self, other): 
+    def __rtruediv__(self, other): return self.__truediv__(other)
+    def __rmul__(self, other): return self.__mul__(other)
+    def __truediv__(self, other): 
+        if isinstance(other, (int, float)): return v(str(self.value / other) + " " + self.unit.get())
+        value = self.value / other.value
+        unit = self.unit.get_div(other)
+        return v(str(value) + " " + unit)
+
+    def __mul__(self, other): 
+        if isinstance(other, (int, float)): return v(str(self.value * other) + " " + self.unit.get())
+        value = self.value * other.value
+        unit = self.unit.get_mul(other)
+        return v(str(value) + " " + unit)
 
     def _get_value(self, value): 
         split = value.split()
-        if len(split) != 2: raise Exception("The input should be 1 number and 1 unit: '5 m', 18.23 m/s, etc.")
+        if len(split) != 2: raise Exception(f"The input should be 1 number and 1 unit: '5 m', '18.23 m/s', etc.\n{value}")
         self.value, self.unit = float(split[0]), Unit(split[1])
 
 ## Unit Class ##
 class Unit():
     def __init__(self, unit) -> None:
         self.unit = unit
+        self.set_power()
+
+        # needs __pow__ method
 
     def check_compatibility(self, other): 
         if self.unit != other.unit.get(): raise Exception(f"Units are not compatible: {self.unit} : {other.unit.get()}")
 
-    def get(self): return self.unit
-    def get_add(self, other): self.check_compatibility(other); return self.unit
-    def get_sub(self, other): self.check_compatibility(other); return self.unit
+    def add_power(self): 
+        if self.power == 0: return ""
+        return self.unit + "**" + str(self.power) if self.power != 1 else self.unit
+
+    def get(self): return self.add_power()
+    def get_add(self, other): self.check_compatibility(other); return self.add_power()
+    def get_sub(self, other): self.check_compatibility(other); return self.add_power()
+
+    def get_mul(self, other):
+        if self.unit == other.unit.unit: 
+            power = self.power + other.unit.power
+            return self.unit + "**" + str(power)
+        return self.add_power() + "*" + other.unit.add_power()
+
+    def get_div(self, other):
+        if self.unit == other.unit.unit: 
+            power = self.power - other.unit.power
+            print("Power: ", power, self.power, other.unit.power)
+            return self.unit + "**" + str(power)
+        return self.add_power() + "/" + other.unit.add_power()
+
+    def set_power(self):
+        self.unit = self.unit.replace("**", "^")
+        print("Unit: ", self.unit, self.unit.split("^"))
+        if "^" in self.unit: self.unit, self.power = self.unit.split("^")
+        else: self.power = 1
+        self.power = int(self.power)
 
     def get_prefix(self):
         if self.unit[1:] in UNITS: self.prefix = PREFIXES[self.unit[0]]; self.unit = self.unit[1:]
--- unit_test.py
+++ unit_test.py
@@ -2,13 +2,14 @@ from python_units import v
 
 value1 = v("5 km")
 value2 = v("519 m")
+factor = value1 / value2
 
 unit_test = [
     value1 + value2 == "5519.0 m",
     value1 - value2 == "4481.0 m",
-    # value1 * value2 == f"{5000 * 519} m**2"
+    value1 * value2 == f"{float(5000 * 519)} m**2",
+    value1 * 2 == f"{float(5000 * 2)} m",
+    value1 * factor == f"{5000 * 9.633911368015415} m"
 ]
-print(value1)
-print(value2)
-print(value1 + value2)
+
 print(unit_test)
