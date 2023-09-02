cb8e89818edefc11591c878ccbf7374154472192
apros7 <lucas.vilsen@gmail.com>
2023-09-02 14:40:03 +0200

    can work with fractions and convert with .to

--- python_units/__init__.py
+++ python_units/__init__.py
@@ -1,5 +1,7 @@
 import math
 import builtins
+from python_units.functions import fraction_decoder
+
 from python_units.constants import UNITS, SPECIAL_UNITS, PREFIXES
 
 ## Relevant constants ##
@@ -13,8 +15,8 @@ def round(value, digits): copy = value.copy(); copy.round(digits); return copy
 ## Value Class ##
 class v():
     def __init__(self, value):
+        self.nominators, self.denominators = fraction_decoder(value)
         self._get_value(value)
-        self.value *= self.unit.get_prefix()
 
     def copy(self): return v(self.__str__())
     def round(self, digits): self.value = builtins.round(self.value, digits)
@@ -40,15 +42,62 @@ class v():
         return v(str(value) + " " + unit)
 
     def _get_value(self, value): 
+        if len(self.nominators) and not self.denominators: self._get_single_value(value)
+        else: self._get_value_fraction(value)
+    
+    def _get_value_fraction(self, value):
+        self.value = 1
+        self.unit = Unit("")
+        for nominator in self.nominators:
+            if len(nominator.split()) == 1: 
+                self.handle_len_1_value(nominator, "mul")
+            else: 
+                value_obj = v(nominator)
+                self.value *= value_obj.value
+                self.unit *= value_obj.unit
+
+        if self.denominators:
+            for denominator in self.denominators:
+                if len(denominator.split()) == 1: 
+                    self.handle_len_1_value(denominator, "div")
+                else: 
+                    value_obj = v(denominator)
+                    self.value /= value_obj.value
+                    self.unit /= value_obj.unit
+
+    def handle_len_1_value(self, value, method):
+        if self.is_hidden_float(value): 
+            if method == "mul": self.value *= value
+            else: self.value /= value
+        else: 
+            if value not in UNITS:
+                value_obj = v(f"1 {value}")
+                if method == "mul": self.value *= value_obj.value; self.unit *= value_obj.unit
+                else: self.value /= value_obj.value; self.unit /= value_obj.unit
+            else:
+                if method == "mul": self.unit *= Unit(value)
+                else: self.unit /= Unit(value)
+
+    def is_hidden_float(self, value):
+        try: float(value); return True
+        except: return False
+
+    def _get_single_value(self, value):
         split = value.split()
-        if len(split) != 2: raise Exception(f"The input should be 1 number and 1 unit: '5 m', '18.23 m/s', etc.\n{value}")
+        if len(split) != 2: raise Exception(f"The input should be 1 number and 1 unit: '5 m', '18.23 m/s', etc.\nYou gave this input: {value}")
         self.value, self.unit = float(split[0]), Unit(split[1])
+        self.value *= self.unit.get_prefix()
+
+    def to(self, desired_unit):
+        desired_unit_obj = v(f"1 {desired_unit}")
+        if self.unit.get() != desired_unit_obj.unit.get(): raise Exception(f"{self.unit.get()} does not match with desired unit: {desired_unit} with the SI-unit: {desired_unit_obj.unit.get()}")
+        return f"{(self.value / desired_unit_obj.value)} {desired_unit}"
 
 ## Unit Class ##
 class Unit():
     def __init__(self, unit) -> None:
         self.unit = unit
-        self.set_power()
+        self.set_power() ## does not work properly
 
     def _check_compatibility(self, other): 
         if self.unit != other.unit.get(): raise Exception(f"Units are not compatible: {self.unit} : {other.unit.get()}")
@@ -57,6 +106,22 @@ class Unit():
         if not power: power = self.power
         if power == 0: return ""
         return self.unit + "**" + str(power) if power != 1 else self.unit
+
+    def __mul__(self, other): 
+        if not isinstance(other, Unit): raise Exception(f"Other must be Unit: is {type(other)}")
+        if self.unit == other.unit: 
+            power = self.power + other.power
+            return self.unit + "**" + str(power)
+        elif len(self.unit) == 0: return other
+        return Unit(self.add_power() + "*" + other.add_power())
+
+    def __truediv__(self, other): 
+        if not isinstance(other, Unit): raise Exception(f"Other must be Unit: is {type(other)}")
+        if self.unit == other.unit: 
+            power = self.power - other.power
+            return self.unit + "**" + str(power)
+        elif len(self.unit) == 0: return other
+        return Unit(self.add_power() + "/" + other.add_power())
 
     def get(self): return self.add_power()
     def get_add(self, other): self._check_compatibility(other); return self.add_power()
@@ -67,12 +132,14 @@ class Unit():
         if self.unit == other.unit.unit: 
             power = self.power + other.unit.power
             return self.unit + "**" + str(power)
+        elif len(self.unit) == 0: return other.unit.get()
         return self.add_power() + "*" + other.unit.add_power()
 
     def get_div(self, other):
         if self.unit == other.unit.unit: 
             power = self.power - other.unit.power
             return self.unit + "**" + str(power)
+        elif len(self.unit) == 0: return other.unit.get()
         return self.add_power() + "/" + other.unit.add_power()
 
     def set_power(self):
